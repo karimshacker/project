f27f9af5ab5b9649588eb8d5f13d03a289605369
Karim Shacker <karimshacker1234@gmail.com>
2025-06-10 06:01:03 +0300

    SCANNER V2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This ensures assets are loaded relative to the current path
   build: {
     target: 'esnext', // or 'es2022'
     sourcemap: true,
